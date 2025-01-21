8f988700daa69d0f48a9df4164f30a85db87f6e9
Stephane Delcroix <stephane@delcroix.org>
2025-01-21 17:41:38 +0100

    more tests

--- src/Controls/tests/Xaml.UnitTests/Issues/Maui21038.xaml.cs
+++ src/Controls/tests/Xaml.UnitTests/Issues/Maui21038.xaml.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls.Core.UnitTests;
 using Microsoft.Maui.Dispatching;
@@ -7,20 +6,13 @@ using Microsoft.Maui.UnitTests;
 using NUnit.Framework;
 
 namespace Microsoft.Maui.Controls.Xaml.UnitTests;
+
+[XamlProcessing(XamlInflator.Runtime, true)]
 [XamlCompilation(XamlCompilationOptions.Skip)]
 public partial class Maui21038
 {
-    public Maui21038()
-    {
-        InitializeComponent();
-    }
+	public Maui21038() => InitializeComponent();
 
-    public Maui21038(bool useCompiledXaml)
-    {
-        //this stub will be replaced at compile time
-    }
-
-    [TestFixture]
     class Test
     {
         [SetUp]
@@ -33,15 +25,21 @@ public partial class Maui21038
         [TearDown] public void TearDown() => AppInfo.SetCurrent(null);
 
         [Test]
-        public void XamlParseErrorsHaveFileInfo([Values(false, true)] bool useCompiledXaml)
+        public void XamlParseErrorsHaveFileInfo([Values] XamlInflator inflator)
         {
-            if (useCompiledXaml)
+            if (inflator == XamlInflator.XamlC)
             {
                 MockCompiler.Compile(typeof(Maui21038), out var md, out var hasLoggedErrors);
                 Assert.That(hasLoggedErrors);
             }                
-            else
-                Assert.Throws<XamlParseException>(() => new Maui21038(useCompiledXaml));
+            else if (inflator == XamlInflator.Runtime)
+                Assert.Throws<XamlParseException>(() => new Maui21038(inflator));
+            else if (inflator == XamlInflator.SourceGen)
+            {
+                var result = MockSourceGenerator.RunMauiSourceGenerator(MockSourceGenerator.CreateMauiCompilation(), typeof(Maui21038));
+                Assert.That(result.Diagnostics, Is.Not.Empty);
+                
+            }
         }
     }
 }
