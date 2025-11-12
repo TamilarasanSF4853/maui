7100f51c3161e3446c3e229043478604655c3233
TamilarasanSF4853 <tamilarasan.velu@syncfusion.com>
2025-11-12 10:26:36 +0530

    added color

--- src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
+++ src/Controls/samples/Controls.Sample.Sandbox/MainPage.xaml.cs
@@ -5,6 +5,7 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		this.SetAppThemeColor(BackgroundColorProperty, Colors.White, Colors.Black);
 	}
 
 	public void OnLightThemeButtonClicked(object sender, EventArgs e)
