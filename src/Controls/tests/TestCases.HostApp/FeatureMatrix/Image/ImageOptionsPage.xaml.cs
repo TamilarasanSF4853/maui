697646633cfd52d59b7d772fe4a8c4581c5a29e8
TamilarasanSF4853 <tamilarasan.velu@syncfusion.com>
2025-12-10 22:13:35 +0530

    added path

--- src/Controls/tests/TestCases.HostApp/FeatureMatrix/Image/ImageOptionsPage.xaml.cs
+++ src/Controls/tests/TestCases.HostApp/FeatureMatrix/Image/ImageOptionsPage.xaml.cs
@@ -150,8 +150,6 @@ public partial class ImageOptionsPage : ContentPage
 
 	private void ClipRadio_CheckedChanged(object sender, CheckedChangedEventArgs e)
 	{
-		// _containerWidth = 350;
-		// _containerHeight = 550;
 		if (e.Value && BindingContext is ImageViewModel vm && sender is RadioButton rb)
 		{
 			vm.Clip = rb.Content?.ToString() switch
@@ -160,24 +158,193 @@ public partial class ImageOptionsPage : ContentPage
 				"Rectangle" => new RectangleGeometry(new Rect(0, 275, 150, 100)),
 				"Ellipse" => new EllipseGeometry(new Point(175, 275), 100, 65),
 				"RoundRectangle" => new RoundRectangleGeometry(new CornerRadius(50), new Rect(0, 275, 150, 100)),
-				"Path" => new PathGeometry(
-							new PathFigureCollection
-							{
-								new PathFigure
-								{
-									StartPoint = new Point(75, 275),
-									IsClosed = true,
-									Segments = new PathSegmentCollection
-									{
-										new LineSegment(new Point(150, 375)),
-										new LineSegment(new Point(0, 375)),
-									}
-								}
-							}
-						),
 				_ => null
 			};
 		}
+	}
+
+	private void PathTypeRadio_CheckedChanged(object sender, CheckedChangedEventArgs e)
+	{
+		if (e.Value && BindingContext is ImageViewModel vm && sender is RadioButton rb)
+		{
+			string pathType = rb.Content?.ToString() ?? "LineSegment";
+			vm.Clip = CreatePathGeometry(pathType);
+		}
+	}
+
+	private PathGeometry CreatePathGeometry(string pathType)
+	{
+		return pathType switch
+		{
+			// LineSegment: Draws straight lines between points
+			"LineSegment" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(150, 20),
+						IsClosed = true,
+						Segments = new PathSegmentCollection
+						{
+							new LineSegment { Point = new Point(280, 180) },
+							new LineSegment { Point = new Point(20, 180) }
+						}
+					}
+				}
+			},
+			// ArcSegment: Draws an elliptical arc between two points
+			"ArcSegment" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(10, 100),
+						IsClosed = false,
+						Segments = new PathSegmentCollection
+						{
+							new ArcSegment
+							{
+								Point = new Point(290, 100),
+								Size = new Size(140, 80),
+								RotationAngle = 0,
+								IsLargeArc = true,
+								SweepDirection = SweepDirection.CounterClockwise
+							}
+						}
+					}
+				}
+			},
+			// BezierSegment: Draws a cubic Bezier curve (2 control points)
+			"BezierSegment" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(10, 100),
+						IsClosed = false,
+						Segments = new PathSegmentCollection
+						{
+							new BezierSegment
+							{
+								Point1 = new Point(100, 0),
+								Point2 = new Point(200, 200),
+								Point3 = new Point(290, 100)
+							}
+						}
+					}
+				}
+			},
+			// QuadraticBezierSegment: Draws a quadratic Bezier curve (1 control point)
+			"QuadraticBezier" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(10, 100),
+						IsClosed = false,
+						Segments = new PathSegmentCollection
+						{
+							new QuadraticBezierSegment
+							{
+								Point1 = new Point(150, 50),
+								Point2 = new Point(290, 100)
+							}
+						}
+					}
+				}
+			},
+			// PolyLineSegment: Draws a series of connected straight lines
+			"PolyLine" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(150, 10),
+						IsClosed = true,
+						Segments = new PathSegmentCollection
+						{
+							new PolyLineSegment
+							{
+								Points = new PointCollection
+								{
+									new Point(180, 80),
+									new Point(260, 80),
+									new Point(200, 130),
+									new Point(230, 200),
+									new Point(150, 160),
+									new Point(70, 200),
+									new Point(100, 130),
+									new Point(40, 80),
+									new Point(120, 80)
+								}
+							}
+						}
+					}
+				}
+			},
+			// PolyBezierSegment: Draws a series of connected cubic Bezier curves
+			"PolyBezier" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(10, 100),
+						IsClosed = false,
+						Segments = new PathSegmentCollection
+						{
+							new PolyBezierSegment
+							{
+								Points = new PointCollection
+								{
+									// First Bezier curve (3 points: control1, control2, end)
+									new Point(100, 50),
+									new Point(150, 50),
+									new Point(200, 100),
+									// Second Bezier curve
+									new Point(250, 150),
+									new Point(200, 150),
+									new Point(290, 100)
+								}
+							}
+						}
+					}
+				}
+			},
+			// PolyQuadraticBezierSegment: Draws a series of connected quadratic Bezier curves
+			"PolyQuadraticBezier" => new PathGeometry
+			{
+				Figures = new PathFigureCollection
+				{
+					new PathFigure
+					{
+						StartPoint = new Point(10, 100),
+						IsClosed = false,
+						Segments = new PathSegmentCollection
+						{
+							new PolyQuadraticBezierSegment
+							{
+								Points = new PointCollection
+								{
+									// First quadratic curve (2 points: control, end)
+									new Point(100, 50),
+									new Point(150, 100),
+									// Second quadratic curve
+									new Point(200, 150),
+									new Point(290, 100)
+								}
+							}
+						}
+					}
+				}
+			},
+			_ => new PathGeometry()
+		};
 	}
 
 }
